fb50fbdba63fac58efab3721627abdf3647ef80b
Fares Kato <fareskato@gmail.com>
2017-03-11 15:34:16 +0300

    use strict

--- use_strict.js
+++ use_strict.js
@@ -1,11 +1,32 @@
 /**
-* Take good look at this example
+ * "use strict" : has Three benefits:
+ *  01- Force us to use variables that declared with var keyword.
+ *  02- don't allow us to use another keyword from next javascript version(like class, let ....etc) as variables
+ *  03- U can't delete variables or function in use strict mode
 */
 
-var theVal  0;
+// "use strict";
 
+/* Benefit 01 */
+
+var theVal = 0;
+// notice we  didn't declare thVal with var
+// so if we use "use strict" then will have error : Uncaught ReferenceError: thVal is not defined
 thVal = 5;
 
 if(theVal > 0){
-  console.log('hwllo there');
+  console.log('hello there');  // false cuz theVal = 0
 }
+
+
+
+/* Benefit 02 */
+var let = "my name";
+console.log(let);
+
+
+/* Benefit 03 */
+
+var father = 'father name'
+delete father;
+
